977aa8e78834b79f5878180544ce34cc7a74277a
Tommy <362172145@qq.com>
2019-11-21 10:46:32 +0800

    opening
    opening page optimization

--- src/openingAnimation.js
+++ src/openingAnimation.js
@@ -1,8 +1,11 @@
 const logo = document.getElementById("openingLogo");
 const opening = document.getElementById("opening");
 logo.addEventListener("animationend", () => {
-  logo.style.animation = "openingAnimationMiddle 1.5s alternate infinite";
+  logo.style.animation = "openingAnimationMiddle 4s forwards";
   setTimeout(() => {
-    opening.style.display = "none";
-  }, 3000);
+    opening.style.animation = "SlowHidden 4s forwards";
+    opening.addEventListener("animationend", () => {
+      opening.style.display = "none";
+    });
+  }, 4000);
 });
